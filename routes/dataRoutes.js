4d48d3ce5adabce84e7717316147dac6728b0d73
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-17 11:48:30 +0545

    Created dataRoutes.js

---
+++ routes/dataRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const dataController = require("../controller/dataController");
+const { authGuard } = require('../middleware/authGurad');
+
+router.post('/create_data', authGuard, dataController.createData);
+
+router.get("/get_data", authGuard, dataController.getData);
+
+router.get("/get_data/:id", authGuard, dataController.getSingleData);
+
+
+router.put("/update_data/:id", authGuard, dataController.updatedata);
+
+router.delete("/delete_data/:id", authGuard, dataController.deletedata);
+
+
+module.exports = router;
+ 
