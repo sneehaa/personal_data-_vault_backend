61fe2fcb36290d1029f81fdaf9df0e8a47cc74ae
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-17 11:46:58 +0545

    created userModel.js

---
+++ model/userModel.js
@@ -0,0 +1,110 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: { 
+    type: Boolean, 
+    default: false
+   },
+  passwordHistory: [
+    {
+      type: String,
+    },
+  ],
+  passwordLastChanged: {
+    type: Date,
+    default: Date.now,
+  },
+  failedLoginAttempts: {
+    type: Number,
+    default: 0,
+  },
+  accountLockedUntil: {
+    type: Date,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  emailVerificationToken: {
+    type: String,
+  },
+  emailVerificationExpires: {
+    type: Date,
+  },
+  
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password =  bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.updatePassword = async function (newPassword) {
+  // Check if the new password is in the history
+  const passwordIsInHistory = this.passwordHistory.some(async (oldPassword) => {
+    return await bcrypt.compare(newPassword, oldPassword);
+  });
+
+  if (passwordIsInHistory) throw new Error('Password reuse is not allowed.');
+
+  // Add current password to history
+  this.passwordHistory.push(this.password);
+
+  // Hash the new password and save it
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(newPassword, salt);
+  this.passwordLastChanged = Date.now();
+
+  await this.save();
+};
+
+
+
+const User = mongoose.model("User", userSchema);
+
+const validateUser = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+
+
+module.exports = { User, validateUser };
